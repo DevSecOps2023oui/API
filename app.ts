c0a3b04f6e6756154659cfbf5948a8dfd0f4cd6c
maxentr <rouaultmaxent@gmail.com>
2023-01-16 16:19:28 +0100

    refactor: log listen

--- app.ts
+++ app.ts
@@ -1,31 +1,31 @@
 import { ControlerData } from "./controller/controllerData";
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-let controllerData:ControlerData = new ControlerData();
+let controllerData: ControlerData = new ControlerData();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-})
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 
+app.get("/", (req, res) => res.send("🏠"));
+app.get("/all", (req, res) => controllerData.GetAll(req, res));
+app.get("/temp", (req, res) => controllerData.GetTemp(req, res));
+app.get("/hydro", (req, res) => controllerData.GetHydro(req, res));
+app.get("/vent", (req, res) => controllerData.GetVent(req, res));
 
-app.get('/', (req, res) => res.send('🏠'));
-app.get('/all',(req,res)=>controllerData.GetAll(req,res));
-app.get('/temp',(req,res)=>controllerData.GetTemp(req,res));
-app.get('/hydro',(req,res)=>controllerData.GetHydro(req,res));
-app.get('/vent',(req,res)=>controllerData.GetVent(req,res));
-
-
-app.listen(3001,()=>{
-    "Serveur listening on port :3001"
-})
-
+app.listen(3001, () => {
+  console.log("Serveur listening on port :3001");
+});
